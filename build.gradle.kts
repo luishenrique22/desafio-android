31b0195d24a4fb2716acb1db0465fb1e3871d0ed
Luis Henrique <luis_ccm14@hotmail.com>
2020-09-15 14:33:44 -0300

    Adicionado ktlint

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,25 @@
+import org.jlleitschuh.gradle.ktlint.reporter.ReporterType
+
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
+
+ktlint {
+    version.set("0.35.0")
+    debug.set(true)
+    verbose.set(true)
+    android.set(false)
+    outputToConsole.set(true)
+    reporters {
+        setOf(ReporterType.PLAIN, ReporterType.CHECKSTYLE)
+    }
+    ignoreFailures.set(true)
+    kotlinScriptAdditionalPaths {
+        include(fileTree("scripts/"))
+    }
+    filter {
+        exclude("**/generated/**")
+        include("**/kotlin/**")
+    }
+}
 
 buildscript {
 
@@ -10,6 +31,7 @@ buildscript {
     dependencies {
         classpath("com.android.tools.build:gradle:4.0.1")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.3.72")
+        classpath("org.jlleitschuh.gradle:ktlint-gradle:9.0.0")
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
     }
@@ -19,13 +41,19 @@ allprojects {
     repositories {
         google()
         jcenter()
+        maven {
+            url = uri("https://plugins.gradle.org/m2/")
+        }
 
     }
 }
 plugins {
     id("de.fayard.buildSrcVersions") version "0.6.1"
+    id("org.jlleitschuh.gradle.ktlint") version "9.0.0"
 
 }
 tasks.register("clean",Delete::class){
     delete(rootProject.buildDir)
 }
+
+apply(plugin = "org.jlleitschuh.gradle.ktlint")
