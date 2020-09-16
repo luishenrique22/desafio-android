748228faeeec9d1ef9c0ecc258c70dd485a0104b
Luis Henrique <luis_ccm14@hotmail.com>
2020-09-16 20:27:30 -0300

    Testes de viewModel

--- app/src/test/java/com/picpay/desafio/android/viewmodel/UsersViewModelTest.kt
+++ app/src/test/java/com/picpay/desafio/android/viewmodel/UsersViewModelTest.kt
@@ -5,16 +5,12 @@ import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.Observer
-import androidx.test.runner.AndroidJUnit4
 import com.nhaarman.mockitokotlin2.any
 import com.nhaarman.mockitokotlin2.mock
-import com.nhaarman.mockitokotlin2.verify
 import com.picpay.desafio.android.commom.data.model.Error
 import com.picpay.desafio.android.commom.data.model.State
-import com.picpay.desafio.android.di.CommomModule
 import com.picpay.desafio.android.di.FeatureModules
 import com.picpay.desafio.android.feature.user.data.model.UserData
-import com.picpay.desafio.android.feature.user.di.UserModule
 import com.picpay.desafio.android.feature.user.domain.GetUserParam
 import com.picpay.desafio.android.feature.user.domain.GetUsersUseCase
 import com.picpay.desafio.android.feature.user.ui.UsersViewModel
@@ -22,21 +18,16 @@ import io.mockk.MockKAnnotations
 import io.mockk.every
 import io.mockk.impl.annotations.MockK
 import io.mockk.mockk
-import org.junit.Assert
-
-import org.junit.Before
-import org.junit.Rule
-import org.junit.Test
+import io.mockk.verify
+import org.junit.*
 import org.junit.rules.TestRule
-import org.junit.runner.RunWith
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.loadKoinModules
 import org.koin.core.context.startKoin
+import org.koin.core.context.stopKoin
 import org.koin.dsl.module
 import org.koin.test.KoinTest
 import org.koin.test.inject
-
-import org.mockito.Mockito
 import org.mockito.MockitoAnnotations
 
 
@@ -53,6 +44,8 @@ class UsersViewModelTest : KoinTest {
     @MockK
     private lateinit var context: Context
 
+    @MockK(relaxed = true)
+    private lateinit var observerResult: Observer<State<List<UserData>>>
     @Before
     fun setup() {
         MockitoAnnotations.initMocks(this)
@@ -76,12 +69,12 @@ class UsersViewModelTest : KoinTest {
             UserData(id = 3, name = "Joao", userName = "@joao", image = "03")
         )
         val expectedResult: LiveData<State<List<UserData>>> =
-            MutableLiveData(State.Success(users)) as LiveData<State<List<UserData>>>
+            MutableLiveData(State.Success(users))
 
         val observer = mock<Observer<State<List<UserData>>>>()
         every { getUsersUseCase.execute(any()) } returns expectedResult
         viewModel.getUsersLiveData().observeForever(observer)
-        getUsersUseCase.execute(any())
+        getUsersUseCase.execute(GetUserParam())
         Assert.assertEquals(expectedResult.value, viewModel.getUsersLiveData().value)
     }
 
@@ -93,12 +86,12 @@ class UsersViewModelTest : KoinTest {
             UserData(id = 3, name = "Joao", userName = "@joao", image = "03")
         )
         val expectedResult: LiveData<State<List<UserData>>> =
-            MutableLiveData(State.Loading(users)) as LiveData<State<List<UserData>>>
+            MutableLiveData(State.Loading(users))
 
         val observer = mock<Observer<State<List<UserData>>>>()
         every { getUsersUseCase.execute(any()) } returns expectedResult
         viewModel.getUsersLiveData().observeForever(observer)
-        getUsersUseCase.execute(any())
+        getUsersUseCase.execute(GetUserParam())
         Assert.assertEquals(expectedResult.value, viewModel.getUsersLiveData().value)
     }
 
@@ -115,12 +108,46 @@ class UsersViewModelTest : KoinTest {
                     Error.NetworkError,
                     users
                 )
-            ) as LiveData<State<List<UserData>>>
+            )
 
         val observer = mock<Observer<State<List<UserData>>>>()
         every { getUsersUseCase.execute(any()) } returns expectedResult
         viewModel.getUsersLiveData().observeForever(observer)
-        getUsersUseCase.execute(any())
+        getUsersUseCase.execute(GetUserParam())
         Assert.assertEquals(expectedResult.value, viewModel.getUsersLiveData().value)
+    }
+
+    @Test
+    fun `GIVEN an valid call viewModel SHOULD return a valid LiveData with state Loading and Success`() {
+        val users: List<UserData> = listOf(
+            UserData(id = 1, name = "Luis", userName = "@luis", image = "01"),
+            UserData(id = 2, name = "Carlos", userName = "@carlos", image = "02"),
+            UserData(id = 3, name = "Joao", userName = "@joao", image = "03")
+        )
+        val expectedResult = MutableLiveData<State<List<UserData>>>()
+        every { getUsersUseCase.execute(any()) } returns expectedResult
+        viewModel.getUsersLiveData().observeForever(observerResult)
+        getUsersUseCase.execute(GetUserParam())
+        expectedResult.value = State.Loading()
+        expectedResult.value = State.Success(users)
+        verify { observerResult.onChanged(State.Loading()) }
+        verify { observerResult.onChanged(State.Success(users)) }
+    }
+
+    @Test
+    fun `GIVEN an valid call viewModel SHOULD return a valid LiveData with state Loading and Error`() {
+        val expectedResult = MutableLiveData<State<List<UserData>>>()
+        every { getUsersUseCase.execute(any()) } returns expectedResult
+        viewModel.getUsersLiveData().observeForever(observerResult)
+        getUsersUseCase.execute(GetUserParam())
+        expectedResult.value = State.Loading()
+        expectedResult.value = State.Failure(Error.NetworkError, null)
+        verify { observerResult.onChanged(State.Loading()) }
+        verify { observerResult.onChanged(State.Failure(Error.NetworkError, null)) }
+    }
+
+    @After
+    fun tearDown(){
+        stopKoin()
     }
 }
